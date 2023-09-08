b3b3cc4ac9a59ed28305e97dac9e6e3145fa4db5
alfazsozib <alfazsozib.com>
2023-09-09 04:54:34 +0600

    profit table added

--- client/src/components/Profit.jsx
+++ client/src/components/Profit.jsx
@@ -8,13 +8,25 @@ import toast, { Toaster } from 'react-hot-toast';
 
 
 function Profit() {
+    const [profits, setProfits] = useState('');
+
+    const getProfitData=async()=>{
+        const data = await axios.get("http://localhost:8080/get-profit-table")
+        setProfits(data.data);
+    }
+
+    useEffect(()=>{
+        getProfitData()
+    },[])
+
+    console.log(profits)
   return (
     <div className='h-screen bg-cyan-500 p-12'>
         <div className='text-white text-3xl font-bold pb-8'>
             <h1>PROFIT TABLE</h1>
         </div>
 
-        <div className='grid grid-cols-4 gap-2'>
+        {/* <div className='grid grid-cols-4 gap-2'>
             <div className='flex flex-col gap-2'>
                 <label className='text-white' htmlFor="metaTraderID">META TRADER ID</label>
                 <input type="text" name="metaId" />
@@ -32,26 +44,73 @@ function Profit() {
                 <input type="text" name="profit" />
             </div>
             <button className='px-6 text-white font-bold py-2 bg-[#121212]'>Submit</button>
-        </div>
+        </div> */}
 
         <div className='pt-12'>
-            <div id='data--section' className='flex items-center border-b-[1px] border-yellow-600 justify-between text-white'>
+            {/* <div id='data--section' className='grid grid-cols-5 gap-10 items-center border-b-[1px] border-yellow-400 text-white'>
                 <div>
-                    <h3>ID</h3>
+                    <h3 className='font-extrabold'>ID</h3>
                 </div>
                 <div>
-                    <h3>BLANCE</h3>
+                    <h3 className='font-extrabold'>BLANCE</h3>
                 </div>
                 <div>
-                    <h3>PROFIT</h3>
+                    <h3 className='font-extrabold'>PROFIT</h3>
                 </div>
                 <div>
-                    <h3>DATE</h3>
+                    <h3 className='font-extrabold'>DATE</h3>
+                </div>
+                <div className=''>
+                   <h3 className='font-extrabold'>Action</h3>
+                </div>
+            </div> */}
+            
+            <table className='w-96'>
+                <tr className='flex gap-80'>
+                    <th>ID</th>
+                    <th>BALANCE</th>
+                    <th>PROFIT</th>
+                    <th>DATE</th>
+                    <th>ACTION</th>
+                </tr>
+                <tbody>
+                    {profits? profits.map((item, index)=>
+                    <tr className='flex pt-2 gap-[340px] text-white font-semibold'>
+                        <td>{item.metaID}</td>
+                        <td>{item.balance}</td>
+                        <td>{item.profit}</td>
+                        <td>{item.date}</td>
+                        <td className='flex gap-2'>
+                            <button onClick={() => ''} className='rounded-lg bg-yellow-400 hover:bg-yellow-500 hover:text-white font-semibold px-4'>Edit</button>
+                            <button onClick={() => ''} className='rounded-lg bg-red-600 hover:bg-red-700 hover:text-white font-semibold px-4'>Delete</button>
+                        </td>
+                    </tr>
+                    
+                    ):null}
+                </tbody>
+            </table>
+
+            <div id='data--section' className='pt-4 grid grid-cols-5 items-center  justify-between text-white'>
+            {/* {profits ? profits.map((item, index)=>
+            <>
+                <div>
+                    <h3>{item.metaID}</h3>
+                </div>
+                <div>
+                    <h3>{item.balance}</h3>
+                </div>
+                <div>
+                    <h3>{item.profit}</h3>
+                </div>
+                <div>
+                    <h3>{item.date}</h3>
                 </div>
                 <div className=''>
                     <button onClick={() => ''} className='rounded-lg hover:bg-yellow-500 hover:text-white font-semibold px-4'>Edit</button>
                     <button onClick={() => ''} className='rounded-lg hover:bg-red-700 hover:text-white font-semibold px-4'>Delete</button>
                 </div>
+                </>
+                ):<h1>Loading ....</h1>} */}
             </div>
         </div>
 
